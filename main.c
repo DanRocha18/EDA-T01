a1a588e2602a18ca6e91f3400b05afe2267b3da7
Pedro Henrique de Deus Vieira <pedro.vieira@comunix.net.br>
2022-02-18 16:09:11 -0300

    Modelo inicial da leitura do arquivo csv

--- main.c
+++ main.c
@@ -1,9 +1,38 @@
-#include<stdio.h>
-#include<string.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void printaCoisas(){
+    printf("Hello World");
+}
 int main(){
 
-    printf("Hello World\n");
+    FILE *file = fopen("tripadvisor_hotel_reviews.csv" , "r");
+    if(file == NULL) {
+        printf("Não foi possível abrir o arquivo\n");
+        exit(1);
+    }
 
+    int cnr;
+    char c;
+    /*while(cnr != EOF){
+        c = fgetc(file);
+        if(c == ','){
+            printf("");
+        }else{
+            printf("%c", c);
+        }
+    }*/
+    char line[200];
+    int nota;
+    char *review;
+    nota = sizeof(line) - 1;
+    printf("%d\n", nota);
+    while(fgets(line , 200 , file) != NULL) {
+        review = strtok(line, "\"");
+        printf("%s\n" , review);
+        review = strtok(NULL , "\"");
+
+    }
+    printaCoisas();
     return 0;
 }
